e3dcc9fbc6fef9f341f383d23efb80e56ba1d9aa
Synergistisitit <synergisticit2020@gmail.com>
2019-02-20 10:17:28 -0800

    edit movie locally using cloning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   //Movie movie=new Movie();
   //instantiating blank object of the Movie
   public movie:Movie=new Movie();
-
+  public buttonTitle="Add Movie"; 
   public tmovies:Movie[]=[];
 
   //This method we want to call  when 
@@ -20,9 +20,31 @@ export class AppComponent {
   public addMovie():void {
     console.log("@)@)@))@(@(@(@");
     console.log(this.movie);
+    const lmovie=this.movie;
+    //Check this object exist of not
+    let update=false;
+    this.tmovies.forEach((t,i)=>{
+        console.log(t);
+        console.log(i);
+        console.log("____updated record!");
+        console.log(lmovie);
+        if(t.title===lmovie.title){
+          update=true;
+          this.tmovies[i]=lmovie;
+          console.log(t);
+          //arr1.map(obj => arr2.find(o => o.id === obj.id) || obj);
+        }
+     });
+
     //adding movie inside movies array....
-    this.tmovies.push(this.movie);
-    this.message="Hey! your movie has been upload successfully....";
+    if(!update) {
+      this.tmovies.push(this.movie);
+      this.message="Hey! your movie has been upload successfully....";
+    }else{
+      this.message="Hey! your movie has been update successfully....";
+    }
+    
+    this.buttonTitle="Add Movie";
     this.movie=new Movie();
   }
 
@@ -30,4 +52,13 @@ export class AppComponent {
     this.tmovies=this.tmovies.filter(t=>t.title!=dmovie.title);
     this.message="Hey! your movie has been deleted successfully....";
   }
+
+  public editMovie(emovie):void {
+    this.buttonTitle="Update Movie";
+    //Creating clone of the edited movie
+    this.movie = Object.assign({}, emovie);
+    //this.movie=emovie;
+  }
+
+  
 }
